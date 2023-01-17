36d689b35cb96958219d798280086e0e1d2bc74a
pdragon <pdragon0512@gmail.com>
2023-01-17 07:35:41 -0800

    again

--- AdvExampleApp/Program.cs
+++ AdvExampleApp/Program.cs
@@ -1,37 +1,103 @@
-﻿using Newtonsoft.Json.Linq;
-using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using AdvLibrary.ForgeApi;
 using AdvLibrary.ForgeApi.Model;
-
-class AutodeskForge {
-    
-
-    // Entry point
-    static void Main(string[] args)
+namespace AdvExampleApp
+{
+    internal class Program
     {
-        // Get the client id and client secret from 3rd party
-        string client_id = "z9AnxOhryxcTSTzyA2oRRJCaiYGIMr6g";
-        string client_secret = "Lq96viASKfBHq83e";
+        static void Main(string[] args)
+        {
 
-        // Create an instance of the class
-        ForgeApi forge = new(client_id, client_secret);
-
-        var hubs = new List<AutoHub>();
-        var projects = new List<AutoProject>();
-        var topFolders = new List<AutoFolder>();
-
-        //Here I will get a list of hubs for the credentials
-        hubs = new List<AutoHub>(forge.GetHubsAsync().Result);
-
-        //Here I will select myHub based on some properties
-        AutoHub myHub = hubs.Where(x => x.HubName == "ADV").First();
-
-        //Here I will pass myHub to get a list of all projects in the Hub
-        projects = new List<AutoProject>(forge.GetProjectsAsync(myHub.HubId);
+//            var designAutomation1 = new DesignAutomation();
 
 
 
+            string client_id = "z9AnxOhryxcTSTzyA2oRRJCaiYGIMr6g";
+            string client_secret = "Mc7fd0f574c8b4e2";
+            var authenticate = new Authentication(client_id, client_secret, "V5MzzOzwgIPMbC3NDzlkyKSPsUYo48I25cUIMzkcPb");
 
 
+            ////////////////////designAutomation part
+            //
+            var dtoken= authenticate.GetDesignAutomationToken();
+            var designAutomation = new DesignAutomation(dtoken);
+ 
+//            designAutomation.CreateNickName("pdragon0512");
+            designAutomation.RegisterAppBundle("DeleteWallsApp5", "Autodesk.Revit+2018", "Delete Walls AppBundle based on Revit 2018");
+            designAutomation.UploadAppBundle("C:\\Users\\SteerC\\Music\\delete\\DeleteWallsApp.zip");
+
+            //var authenticate = new Authentication(client_id, client_secret);
+
+            /*
+             ///if you use refresh token you can use like this            
+             var authenticate = new Authentication(client_id, client_secret, refresh_token);
+             ex:  var authenticate = new Authentication(client_id, client_secret, "V5MzzOzwgIPMbC3NDzlkyKSPsUYo48I25cUIMzkcPb");
+            */
+
+            ///////////////////designAutomation part
+            var refreshtoken = authenticate.Refresh_token;
+
+            
+            var forge = new DataManagement(authenticate.Token);
+
+            var hubs = new List<AutoHub>();
+            var projects = new List<AutoProject>();
+            var folders = new List<AutoFolder>();
+            var files = new List<AutoFile>();
+
+            //Here I will get a list of hubs and select myHub based on some properties
+            hubs = new List<AutoHub>(forge.GetHubsAsync().Result);
+            AutoHub myHub = hubs.Where(x => x.HubName == "ADV").First();
+
+
+           
+            //Here I will pass myHub to get a list of all projects in the Hub and select a project
+            projects = new List<AutoProject>(forge.GetProjectsAsync(myHub.HubId).Result);
+            AutoProject myProject = projects.Where(x => x.ProjectName == "Emek Hospital").First();
+         
+           
+
+             //Here I will pass myProject to get list of all folder paths in the project
+             folders = new List<AutoFolder>(forge.GetFoldersAsync(myHub.HubId,myProject.ProjectId).Result);
+
+
+             AutoFolder myFolder = folders.Where(x => x.FolderName == "Project Files").First();
+           
+            //Here I will pass myFolder to get list of all files in the folder and select a revit model
+            files = new List<AutoFile>(forge.GetFilesAsync(myProject.ProjectId,myFolder.FolderId).Result);
+            AutoFile myFile = files.Where(x => x.Name == "Emek_Hospital_Superposition_2021_V2.rvt").First();
+
+            /*
+            var flag = false;
+            while (!flag)
+            {
+                flag = forge.HasFinishedPublishingAsync(myProject.ProjectId, myFolder.FolderId, myFile.ContentId);
+            }*/
+            try
+            {
+                bool needPublish = forge.HasFinishedPublishingAsync(myProject.ProjectId, myFolder.FolderId, myFile.ContentId);
+                /*      if (needPublish)
+                      {
+                            var result = forge.DoPublishLatestAsync(myProject.ProjectId, myFolder.FolderId, myFile.ContentId);
+                              //if committed
+                            if(result== "committed")
+                            {
+                              var flag = false;
+                              while (!flag)
+                              {
+                                  flag=forge.HasFinishedPublishingAsync(myProject.ProjectId, myFolder.FolderId, myFile.ContentId);
+                              }
+                            }
+                       }*/
+            }
+            catch(Exception e)
+            {
+                var error = e.ToString();
+                Console.WriteLine(error);
+            }
+
+        }
     }
 }
--- AdvLibrary/ForgeApi/DesignAutomation.cs
+++ AdvLibrary/ForgeApi/DesignAutomation.cs
@@ -40,7 +40,6 @@ namespace AdvLibrary.ForgeApi
         private string token;
         private DateTime lastTokenTime;
         private int expiresIn;
-
         private string nickName;
         #endregion
 
@@ -95,7 +94,7 @@ namespace AdvLibrary.ForgeApi
 
             return false;
         }
-
+        #region ForTask3
         ///create Nickname for app
         public bool CreateNickName(string nickname)
         {
@@ -113,24 +112,163 @@ namespace AdvLibrary.ForgeApi
             {
                 streamWriter.Write(Newtonsoft.Json.JsonConvert.SerializeObject(data).ToString());
             }
-
-            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
-            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            try
             {
-                var result = streamReader.ReadToEnd();
+                var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+                
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                }
+                if (httpResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    this.nickName = nickname;
+                    return true;
+                }
+                if (httpResponse.StatusCode == HttpStatusCode.Conflict)
+                {
+                    /// in case of conflict, so change the name
+                    return false;
+                }
             }
-            if(httpResponse.StatusCode == HttpStatusCode.OK)
+            catch(Exception e)
             {
-                return true;
-            }
-            if(httpResponse.StatusCode == HttpStatusCode.Conflict)
-            {
-                /// in case of conflict, so change the name
+                Console.WriteLine("You have data for your nickname. Cann't patch. You have to delete first");
                 return false;
             }
-
             return false;
         }
+        #endregion
+
+        #region ForTask4
+
+        private string Policy;
+        private string x_amz_signature;
+        private string x_amz_credential;
+        private string x_amz_algorithm;
+        private string x_amz_date;
+        private string x_amz_security_token;
+        private string AppId;
+        private string AppEngine;
+        private string EndPointUrl;
+        private string version;
+        private string AppKey;
+        public bool RegisterAppBundle(string appId, string appEngine,string appDescription)
+        {
+
+            var url = "https://developer.api.autodesk.com/da/us-east/v3/appbundles";
+            string jsonResponse = string.Empty;
+
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Method = "POST";
+
+            httpRequest.Headers["Authorization"] = "Bearer " + token;
+            httpRequest.ContentType = "application/json";
+
+            var data = new { 
+                id = appId,
+                engine = appEngine,
+                description = appDescription
+            };
+
+            using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
+            {
+                streamWriter.Write(Newtonsoft.Json.JsonConvert.SerializeObject(data).ToString());
+            }
+            try
+            {
+                var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    jsonResponse = streamReader.ReadToEnd();
+                }
+
+                JObject json = JObject.Parse(jsonResponse);
+                if (!string.IsNullOrEmpty(json["id"]?.ToString()))
+                {
+                    Policy = json["uploadParameters"]["formData"]["policy"].ToString();
+                    x_amz_signature = json["uploadParameters"]["formData"]["x-amz-signature"].ToString();
+                    x_amz_credential = json["uploadParameters"]["formData"]["x-amz-credential"].ToString();
+                    x_amz_algorithm = json["uploadParameters"]["formData"]["x-amz-algorithm"].ToString();
+                    x_amz_date = json["uploadParameters"]["formData"]["x-amz-date"].ToString();
+                    x_amz_security_token = json["uploadParameters"]["formData"]["x-amz-security-token"].ToString();
+                    AppKey = json["uploadParameters"]["formData"]["key"].ToString();
+                    AppId = json["id"].ToString();
+                    AppEngine = json["engine"].ToString();
+                    EndPointUrl = json["uploadParameters"]["endpointURL"].ToString();
+                    version = json["version"].ToString();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.WriteLine("You have that name app. Can't create again");
+            }
+            return false;
+
+        }
+        
+        public bool  UploadAppBundle(string filepath)
+        {
+            var content =new MultipartFormDataContent();
+            List<KeyValuePair<string, string>> postData = new List<KeyValuePair<string, string>>();
+            
+            postData.Add(new KeyValuePair<string, string>("key", AppKey));
+            postData.Add(new KeyValuePair<string, string>("content-type", "application/octet-stream"));
+            postData.Add(new KeyValuePair<string, string>("policy", Policy));
+            postData.Add(new KeyValuePair<string, string>("success_action_status", "200"));
+            postData.Add(new KeyValuePair<string, string>("success_action_redirect", ""));
+            postData.Add(new KeyValuePair<string, string>("x-amz-signature", x_amz_signature));
+            postData.Add(new KeyValuePair<string, string>("x-amz-credential", x_amz_credential));
+            postData.Add(new KeyValuePair<string, string>("x-amz-algorithm", x_amz_algorithm));
+            postData.Add(new KeyValuePair<string, string>("x-amz-date", x_amz_date));
+            postData.Add(new KeyValuePair<string, string>("x-amz-server-side-encryption", "AES256"));
+            postData.Add(new KeyValuePair<string, string>("x-amz-security-token", x_amz_security_token));
+
+            for (var i=0;i<postData.Count;i++)
+            {
+                string inputName = postData[i].Key;
+                string value = postData[i].Value;
+
+                content.Add(new StringContent(value), inputName);
+            }
+            
+            FileStream fileStream = File.OpenRead(filepath);
+            var streamContent = new StreamContent(fileStream);
+            var fileContent = new ByteArrayContent(streamContent.ReadAsByteArrayAsync().Result);
+            content.Add(fileContent, "file", Path.GetFileName(filepath));
+            
+            string jsonResponse = string.Empty;
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Cache-Control", "no-cache");
+
+            try
+            {
+                HttpResponseMessage result1 = client.PostAsync("https://dasprod-store.s3.amazonaws.com", content).GetAwaiter().GetResult();
+                jsonResponse = result1.Content.ReadAsStringAsync().Result;
+                if(result1.StatusCode == HttpStatusCode.OK)
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine(jsonResponse);
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.WriteLine("You have that name app. Can't create again");
+            }
+            return false;
+        }
+        #endregion
         #endregion
     }
 }
