01682928c0c0d3639fa656b16b21b6b124cec986
pdragon <pdragon0512@gmail.com>
2023-01-21 05:52:14 -0800

    sa

--- AdvExampleApp/Program.cs
+++ AdvExampleApp/Program.cs
@@ -15,33 +15,53 @@ namespace AdvExampleApp
             var authenticate = new Authentication(client_id, client_secret, "V5MzzOzwgIPMbC3NDzlkyKSPsUYo48I25cUIMzkcPb");
 
             /////generating s3 url part for addin
- 
+            ///
+            
+            DataManagement forge = new DataManagement(authenticate.Token);
+
+            var hubs = new List<AutoHub>();
+            var projects = new List<AutoProject>();
+            var topFolders = new List<AutoFolder>();
+            var files = new List<AutoFile>();
+            //Here I will get a list of hubs for the credentials
+            hubs = new List<AutoHub>(forge.GetHubsAsync().Result);
+
+            AutoHub myHub = hubs.Where(x => x.HubName == "ADV").First();
+
+            projects = new List<AutoProject>(forge.GetProjectsAsync(myHub.HubId).Result);
+
+            AutoProject myProject = projects.Where(x => x.ProjectName == "Test Project").First();
+
+            files = new List<AutoFile>(forge.GetFilesAsync(myProject.ProjectId, "urn:adsk.wipprod:fs.folder:co.rhsuy37DRNKKwuuAn6buFQ").Result);
+            AutoFile myFile = files.Where(x => x.Name == "GYT-ZIV-ZZ-ZZ-M3-S-0001.rvt").First();
+
+            var link = forge.FindStorageLocation(myProject.ProjectId, myFile.ContentId);
+
+            var s3url = forge.GetS3Url(link);
+            
             /////// end generating s3 url
             ///
 
 
-            ///////////////////////
-            ////////////////////
+
             ////////////////here, start the design automation
             var dtoken = authenticate.GetDesignAutomationToken();
             var designAutomation = new DesignAutomation(dtoken);
-            //    var designAutomation = new DesignAutomation();
-
             /////create your nickname
-            //  designAutomation.CreateNickName("pdragon");
+            designAutomation.CreateNickName("pdragon1");
             /////
 
-            var nickname = "pdragon";
+            var nickname = "pdragon1";
 
             ///////task 4
 
             var existiedBundle = false;
-            var appId = "CountSampleApps5";
-            var alias = "test5";
+            var appId = "new2";
+            var alias = "test2";
             
             if (designAutomation.RegisterAppBundle(appId, "Autodesk.Revit+2022", "Count AppBundle based on Revit 2022"))
             {
-                if (designAutomation.UploadAppBundle("C:\\test\\DeleteWallsApp.zip"))
+                if (designAutomation.UploadAppBundle("D:\\ttttt\\CountIt.zip"))
                 {
                     Console.WriteLine("uploaded");
                 }
@@ -75,8 +95,8 @@ namespace AdvExampleApp
 
 
             /////for task 5
-            var activityAlias = "countittest3";
-            var activityId = "countittestactivity3";
+            var activityAlias = "newalias";
+            var activityId = "newactivity";
             var activitycreated = false;
             
           if (designAutomation.CreateNewActivity(nickname, appId, alias, activityId, "Autodesk.Revit+2022"))
@@ -111,7 +131,7 @@ namespace AdvExampleApp
 
             ///
             ///The Bucket Key must be unique throughout all of the OSS service. 
-            string bucketname = "pdragon0512bucket"; //// must be [a-z],[0-9],[_]
+            string bucketname = "pdragon0512bucket1"; //// must be [a-z],[0-9],[_]
             string signedUrlForUpload = "";
             string uploadUrlResponseKey = "";
             var objectKey = "result";
@@ -120,13 +140,13 @@ namespace AdvExampleApp
             signedUrlForUpload = designAutomation.GenerateSignedS3Url(bucketname, objectKey, out uploadUrlResponseKey);
 
             var ojbectId = "";
-            var downloadUrl = "";
-//            var downloadUrl = s3url;
+    //        var downloadUrl = "";
+            var downloadUrl = s3url;
             var uploadUrl = "";
 
-        //    uploadUrl = designAutomation.GetUploadUrl(bucketname, objectKey);
-            
-            if (designAutomation.UploadFileToSignedUrl(signedUrlForUpload, "C:\\test\\DeleteWalls.rvt"))
+            uploadUrl = designAutomation.GetUploadUrl(bucketname, objectKey);
+            /*
+            if (designAutomation.UploadFileToSignedUrl(signedUrlForUpload, "D:\\ttttt\\Correlation-ST_2022.rvt"))
             {
                 ojbectId = designAutomation.CompleteUploading(bucketname, objectKey, uploadUrlResponseKey);
                 if (!string.IsNullOrEmpty(ojbectId))
@@ -135,7 +155,7 @@ namespace AdvExampleApp
                     uploadUrl = designAutomation.GetUploadUrl(bucketname, objectKey);
                 }
             }
-            
+           */
             ////start task 7
 
             var itemstatus = "";
@@ -152,6 +172,12 @@ namespace AdvExampleApp
                 {
                     itemstatus = designAutomation.CheckStatusOfItem(workId);
                 }
+            }
+
+            if(itemstatus == "success")
+            {
+                var finalresponse=designAutomation.GetResultString(bucketname, objectKey);
+                Console.WriteLine(finalresponse);
             }
 
         }
